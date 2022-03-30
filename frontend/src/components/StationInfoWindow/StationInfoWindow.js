9b44b34ddef05bed72e39cbfb267637631de9ca6
Paulo <paulozamora2000@gmail.com>
2022-03-30 01:03:40 -0600

    Styling for info window

--- frontend/src/components/StationInfoWindow/StationInfoWindow.js
+++ frontend/src/components/StationInfoWindow/StationInfoWindow.js
@@ -2,6 +2,12 @@ import React from "react";
 import InfoWindow from "react-google-maps/lib/components/InfoWindow";
 import { useNavigate } from "react-router-dom";
 
+import humidityIcon from "../../assets/icons/humidity.svg";
+import pressureIcon from "../../assets/icons/pressure.svg";
+import sunIcon from "../../assets/icons/sun.svg";
+import waterIcon from "../../assets/icons/waterLevel.svg";
+import "./stationInfoWindow.css";
+
 const StationInfoWindow = ({
   station,
   defaultStation,
@@ -18,17 +24,50 @@ const StationInfoWindow = ({
   return (
     <InfoWindow {...props}>
       <div>
-        <h1> {station.name} </h1>
-        {station.id === defaultStation && <p>Default Station</p>}
+        <div className="stationInfo-header">
+          <h1> {station.name} </h1>
+        </div>
 
-        <h2> {`Temperatura: ${station.temperature} °C `} </h2>
-        <h2> {`Humedad: ${station.humidity} %`} </h2>
-        <h2> {`Milimetros: ${station.waterLevel} mm`} </h2>
-        <h2> {`Presion: ${station.pressure} hPa `} </h2>
-        <h2> {`Intensidad Luminica: ${station.light} lux`} </h2>
-        <button onClick={onStationSelected}>
-          Establecer como predeterminada
-        </button>
+        <div className="stationInfo-body">
+          <h2 className="stationInfo-temperature">
+            {" "}
+            {station.temperature} °C{" "}
+          </h2>
+          <div className="stationInfo-row">
+            <div className="stationInfo-value">
+              <img src={humidityIcon} alt="humidity" />
+              <h2>{station.humidity}%</h2>
+            </div>
+
+            <div className="stationInfo-value">
+              <img src={pressureIcon} alt="pressure" />
+              <h2>{station.pressure} hPa</h2>
+            </div>
+          </div>
+
+          <div className="stationInfo-row">
+            <div className="stationInfo-value">
+              <img src={waterIcon} alt="rain" />
+              <h2>{station.waterLevel} cm</h2>
+            </div>
+
+            <div className="stationInfo-value">
+              <img src={sunIcon} alt="light intensity" />
+              <h2>{station.light} lux</h2>
+            </div>
+          </div>
+        </div>
+
+        {station.id !== defaultStation ? (
+          <button
+            className="stationInfo-setDefaultBtn"
+            onClick={onStationSelected}
+          >
+            Set as default station
+          </button>
+        ) : (
+          <p className="stationInfo-chip">This is your default station</p>
+        )}
       </div>
     </InfoWindow>
   );
